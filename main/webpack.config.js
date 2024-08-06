6f5993a165a046d2be7cb18d4d4c16d3e49c1477
thoth2357 <thoth2357@gmail.com>
2024-08-06 04:28:16 +0100

    🩹 Added domain to allowed host

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -3,70 +3,71 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
+	output: {
+		publicPath: "http://localhost:3000/",
+	},
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+	},
 
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-  },
+	devServer: {
+		port: 3000,
+		historyApiFallback: true,
+		allowedHosts: ["customerportal.gworkspace.withhordanso.com"],
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.m?js/,
+				type: "javascript/auto",
+				resolve: {
+					fullySpecified: false,
+				},
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "main",
-      filename: "remoteEntry.js",
-      remotes: {
-        store:"store@http://localhost:3030/remoteEntry.js",
-        auth: "auth@http://localhost:3002/remoteEntry.js",
-        domains: "domains@http://localhost:3001/remoteEntry.js",
-      },
-      exposes: {
-        "./Navbar": "./src/components/Navbar.tsx",
-        "./Footer": "./src/components/Footer.tsx",
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv(),
-  ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "main",
+			filename: "remoteEntry.js",
+			remotes: {
+				store: "store@http://localhost:3030/remoteEntry.js",
+				auth: "auth@http://localhost:3002/remoteEntry.js",
+				domains: "domains@http://localhost:3001/remoteEntry.js",
+			},
+			exposes: {
+				"./Navbar": "./src/components/Navbar.tsx",
+				"./Footer": "./src/components/Footer.tsx",
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+		}),
+		new Dotenv(),
+	],
 });
--- store/webpack.config.js
+++ store/webpack.config.js
@@ -3,69 +3,70 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
-  output: {
-    publicPath: "http://localhost:3030/",
-  },
+	output: {
+		publicPath: "http://localhost:3030/",
+	},
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+	},
 
-  devServer: {
-    port: 3030,
-    historyApiFallback: true,
-  },
+	devServer: {
+		port: 3030,
+		historyApiFallback: true,
+		allowedHosts: ["customerportal.gworkspace.withhordanso.com"],
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.m?js/,
+				type: "javascript/auto",
+				resolve: {
+					fullySpecified: false,
+				},
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "store",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./ReduxProvider": "./src/provider/index.tsx",
-        "./user.storage": "./src/localStorage/user.storage.tsx",
-        "./hooks": "./src/hooks.ts",
-        "./user.thunk": "./src/thunks/user.thunk.tsx",
-        "./authSlice": "./src/slices/authSlice.ts",
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "store",
+			filename: "remoteEntry.js",
+			remotes: {},
+			exposes: {
+				"./ReduxProvider": "./src/provider/index.tsx",
+				"./user.storage": "./src/localStorage/user.storage.tsx",
+				"./hooks": "./src/hooks.ts",
+				"./user.thunk": "./src/thunks/user.thunk.tsx",
+				"./authSlice": "./src/slices/authSlice.ts",
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+		}),
+		new Dotenv(),
+	],
 });
