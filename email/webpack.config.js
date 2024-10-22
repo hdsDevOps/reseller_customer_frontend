17317e5c0143dd8376d5e409df0c76fd777d0139
saunak99 <saunak99@gmail.com>
2024-10-22 14:51:27 +0530

    changes in files for update to run the code

--- email/webpack.config.js
+++ email/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (_, argv) => ({
 
   devServer: {
     port: 3003,
+    allowedHosts: ["all"],
     historyApiFallback: true,
     watchFiles: [path.resolve(__dirname, 'src')],
     onListening: function (devServer) {
