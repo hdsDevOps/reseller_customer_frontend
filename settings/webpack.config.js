48e253fb9ae3c4e6c606701bd19daa98b433e2e3
saunak99 <saunak99@gmail.com>
2024-10-22 10:49:49 +0530

    changes in files for update

--- settings/webpack.config.js
+++ settings/webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3006/",
+    publicPath: "auto",
   },
 
   resolve: {
