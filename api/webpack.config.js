4f8be28d2fb54508bf9f54ad033cf0a3f00e5a5d
saunak99 <saunak99@gmail.com>
2024-11-06 16:51:28 +0530

    changes in API

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-
+ 
 const deps = require("./package.json").dependencies;
 
 const printCompilationMessage = require('./compilation.config.js');
